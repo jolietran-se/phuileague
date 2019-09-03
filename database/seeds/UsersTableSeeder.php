26dfed1fa1e613f51dfbdbadb179d5dffc9a5da7
Phuong Tran <phuongtran99.k60@gmail.com>
2019-09-03 14:48:43 +0700

    first commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // DB::table('users')->insert([
+        //     'name' => Str::random(10),
+        //     'email' => Str::random(10).'@gmail.com',
+        //     'password' => bcrypt('password'),
+        // ]);
+
+        DB::table('users')->insert([
+            'name' => 'Phuong tran',
+            'email' => 'phuongtran99.k60@gmail.com',
+            'password' => bcrypt('123456'),
+        ]);
+    }
+}
