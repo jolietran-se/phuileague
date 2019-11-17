9b0a01be1adf930edff6b953da8000bfff6ef7d6
Phuong Tran <phuongtran99.k60@gmail.com>
2019-11-18 05:39:44 +0700

    views.home

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,65 +5,20 @@
                 <div class="row">
                     <div class="col-md-6">
                         <h4 class="footer-title">{{ trans('footer.about_us') }}</h4>
-                            <div class="about-widget">
-                                Hệ thống quản lý giải đấu chuyên nghiệp<br>
-                                Chúng tôi rất vui vì được đồng hành bạn tạo ra những giải đấu tuyệt vời!
-                            </div>
+                        <div class="about-widget">
+                            Hệ thống quản lý giải đấu chuyên nghiệp<br>
+                            Chúng tôi rất vui vì được đồng hành bạn tạo ra những giải đấu tuyệt vời!
+                        </div>
                     </div>
                     <div class="col-md-6">
                         <h4 class="footer-title">{{ trans('footer.owner') }}</h4>
-                            <div class="about-widget">
-                                MSSV: 20156275<br>
-                                Lớp: CN CNTT 01.K60<br>
-                                Mail: phuong.tt156275@sis.hust.edu.vn<br>
-                                Trường Đại học Bách Khoa Hà Nội<br>
-                            </div>
-                    </div>
-                    {{-- </div>
-                    <div class="col-md-3">
-                        <h4 class="footer-title">Recent Posts</h4>
-                        <div class="recent-post-widget">
-                            <div class="post-item">
-                                <div class="post-date">
-                                    <span>28</span>
-                                    <span>June</span>
-                                </div>
-                                <div class="post-desc">
-                                    <h5 class="post-title"><a href="blog-single.html">Welcome to Soccer Template</a></h5>
-                                    <span class="post-category">Design</span>
-                                </div>
-                            </div>
-                            <div class="post-item">
-                                <div class="post-date">
-                                    <span>30</span>
-                                    <span>July</span>
-                                </div>
-                                <div class="post-desc">
-                                    <h5 class="post-title"><a href="blog-single.html">Welcome to sports template</a></h5>
-                                    <span class="post-category">Design</span>
-                                </div>
-                            </div>
+                        <div class="about-widget">
+                            MSSV: 20156275<br>
+                            Lớp: CN CNTT 01.K60<br>
+                            Mail: phuong.tt156275@sis.hust.edu.vn<br>
+                            Trường Đại học Bách Khoa Hà Nội<br>
                         </div>
                     </div>
-                    <div class="col-md-3">
-                        <h4 class="footer-title">Useful Links</h4>
-                        <ul class="sitemap-widget">
-                            <li class="active"><a href="about.html">Home</a></li>
-                            <li><a href="about.html">About</a></li>
-                            <li><a href="point-table.html">Point Table</a></li> 
-                                           <li><a href="world-cup.html">World Cup</a></li> 
-                            <li><a href="blog.html">Blog</a></li>
-                            <li><a href="contact.html">Contact</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-3">
-                        <h4 class="footer-title">newsletter</h4>
-                        <form class="footer-subscribe">
-                            <input type="text" class="form-input" placeholder="Name">
-                            <input type="email" class="form-input" placeholder="Email">
-                            <input type="submit" class="form-input" value="Send">
-                        </form>
-                    </div> --}}
                 </div>
             </div>
         </div>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,56 +1,3 @@
-{{-- <header>
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
-        </div>
-    </nav>
-</header> --}}
-
 <header>
     <div class="header-top-area">
         <div class="container">
@@ -99,7 +46,7 @@
                         <nav class="rs-menu">
                             <ul class="nav-menu">
                                 <!-- Home -->
-                                <li class="current-menu-item current_page_item"><a href="#">{{ trans('header.home') }}</a></li>
+                                <li class="current-menu-item current_page_item"><a href="{{ route('home') }}">{{ trans('header.home') }}</a></li>
                                 
                                 <!-- Tìm giải đấu -->
                                 <li><a href="#">{{ trans('header.tournament_search') }}</a></li>
@@ -122,9 +69,9 @@
                                 </li>
                                 <!-- Authentication Links -->
                                 @guest
-                                    <a href="{{ route('login') }}">{{ trans('header.login') }}</a>
+                                    <li><a href="{{ route('login') }}">{{ trans('header.login') }}</a></li>
                                     @if (Route::has('register'))
-                                        <a href="{{ route('register') }}">{{ trans('header.register') }}</a>
+                                        <li><a href="{{ route('register') }}">{{ trans('header.register') }}</a></li>
                                     @endif
                                 @else
                                     <li class="menu-item-has-children">
@@ -148,7 +95,7 @@
                                 @endguest
 
                                 <!-- Liên hệ --> 
-                                <li><a href="#">{{ trans('header.contact') }}</a></li>
+                                {{-- <li><a href="#">{{ trans('header.contact') }}</a></li> --}}
                             </ul>
                        </nav>
                    </div>
